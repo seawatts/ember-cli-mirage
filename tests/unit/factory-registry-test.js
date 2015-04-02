f46ab87308c32dd55e810ba006735cac48a0ae4d
Sam Selikoff <sam.selikoff@gmail.com>
2015-04-02 06:35:32 -0400

    working..

--- tests/unit/factory-registry-test.js
+++ tests/unit/factory-registry-test.js
@@ -22,29 +22,29 @@ test('it errors when attempting to building an unknown definition', function(ass
   throws(() => registry.create('contact'));
 });
 
-test('it supports one to one associations', function() {
-  registry.register('contact', Mirage.Factory.extend({
-    name: 'Sam',
-    address: Mirage.hasOne('address')
-  }));
+// test('it supports one to one associations', function() {
+//   registry.register('contact', Mirage.Factory.extend({
+//     name: 'Sam',
+//     address: Mirage.hasOne('address')
+//   }));
 
-  registry.register('address', Mirage.Factory.extend({
-    street: '123 Way',
-    city: 'Hyrule'
-  }));
+//   registry.register('address', Mirage.Factory.extend({
+//     street: '123 Way',
+//     city: 'Hyrule'
+//   }));
 
-  var contact = registry.create('contact');
+//   var contact = registry.create('contact');
 
-  deepEqual(contact, {
-    id: 1,
-    name: 'Sam',
-    address: {
-      id: 1,
-      contact_id: 1,
-      street: '123 Way',
-      city: 'Hyrule'
-    }
-  });
-  deepEqual(db.contacts, [{id: 1, name: 'Sam'}]);
-  deepEqual(db.addresses, [{id: 1, contact_id: 1, street: '123 Way', city: 'Hyrule'}]);
-});
+//   deepEqual(contact, {
+//     id: 1,
+//     name: 'Sam',
+//     address: {
+//       id: 1,
+//       contact_id: 1,
+//       street: '123 Way',
+//       city: 'Hyrule'
+//     }
+//   });
+//   deepEqual(db.contacts, [{id: 1, name: 'Sam'}]);
+//   deepEqual(db.addresses, [{id: 1, contact_id: 1, street: '123 Way', city: 'Hyrule'}]);
+// });
