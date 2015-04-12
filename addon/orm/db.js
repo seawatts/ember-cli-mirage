cb7581264439b8085a6006b254698580f315d585
Sam Selikoff <sam.selikoff@gmail.com>
2015-04-12 15:28:28 -0400

    Update db api to use #all; ensure only returns copies

--- addon/orm/db.js
+++ addon/orm/db.js
@@ -1,26 +1,26 @@
 import Ember from 'ember';
 
 /*
-  An identity map.
+  The db, an identity map.
+
+  Note the public methods return copies of the data,
+  so the actual db records cannot be inadvertantly
+  modified.
 */
 export default function() {
 
   this.createCollection = function(collection) {
     var _this = this;
 
-    this[collection] = [];
+    this[collection] = {
+      _records: []
+    };
 
     // Attach the methods to the collection
-    // TODO: use prototype
-    ['insert', 'find', 'where', 'update', 'remove']
+    // TODO: use prototype?
+    ['all', 'insert', 'find', 'where', 'update', 'remove']
       .forEach(function(method) {
-        _this[collection][method] = function() {
-          var args = [collection];
-          for (var i = 0; i < arguments.length; i++) {
-            args.push(arguments[i]);
-          }
-          return _this['_' + method].apply(_this, args);
-        };
+        _this[collection][method] = _this[method].bind(_this, collection);
       });
 
     return this;
@@ -40,140 +40,9 @@ export default function() {
 
     Ember.keys(data).forEach(function(collection) {
       _this.createCollection(collection);
-      _this._insert(collection, data[collection]);
+      _this.insert(collection, data[collection]);
     });
   };
-
-  this._insert = function(collection, data) {
-    var _this = this;
-    var copy = data ? JSON.parse(JSON.stringify(data)) : {};
-    var returnData;
-
-    if (!Ember.isArray(copy)) {
-      var attrs = copy;
-      if (!attrs.id) {
-        attrs.id = _this[collection].length + 1;
-      }
-
-      _this[collection].push(attrs);
-      returnData = attrs;
-
-    } else {
-      returnData = [];
-      copy.forEach(function(attrs) {
-        if (!attrs.id) {
-          attrs.id = _this[collection].length + 1;
-        }
-
-        _this[collection].push(attrs);
-        returnData.push(attrs);
-      });
-    }
-
-    return returnData;
-  };
-
-  this._find = function(collection, ids) {
-    var _this = this;
-
-    if (Ember.isArray(ids)) {
-      return ids.map(function(id) {
-        return _this._findRecordForId(collection, id);
-      }).filter(function(record) {
-        return record !== undefined;
-      });
-
-    } else {
-      return this._findRecordForId(collection, ids);
-    }
-  };
-
-  this._findRecordForId = function(collection, id) {
-    // If parses, coerce to integer
-    id = parseInt(id, 10) || id;
-
-    return this[collection].filter(function(obj) {
-      return obj.id === id;
-    })[0];
-  };
-
-  this._where = function(collection, query) {
-    var records = this[collection];
-
-    Object.keys(query).forEach(function(queryKey) {
-      records = records.filter(function(record) {
-        return record[queryKey] === query[queryKey];
-      });
-    });
-
-    return records;
-  };
-
-  this._update = function(collection, attrs, target) {
-    if (typeof target === 'undefined') {
-      this[collection].forEach(function(record) {
-        Object.keys(attrs).forEach(function(attr) {
-          record[attr] = attrs[attr];
-        });
-      });
-
-    } else if (typeof target === 'number') {
-      var id = target;
-      var record = this._find(collection, id);
-
-      Object.keys(attrs).forEach(function(attr) {
-        record[attr] = attrs[attr];
-      });
-
-    } else if (Ember.isArray(target)) {
-      var ids = target;
-      var records = this._find(collection, ids);
-
-      records.forEach(function(record) {
-        Object.keys(attrs).forEach(function(attr) {
-          record[attr] = attrs[attr];
-        });
-      });
-
-    } else if (typeof target === 'object') {
-      var query = target;
-      var records = this._where(collection, query);
-
-      records.forEach(function(record) {
-        Object.keys(attrs).forEach(function(attr) {
-          record[attr] = attrs[attr];
-        });
-      });
-    }
-  };
-
-  this._remove = function(collection, target) {
-    var _this = this;
-
-    if (typeof target === 'undefined') {
-      this[collection] = [];
-
-    } else if (typeof target === 'number') {
-      var record = this._find(collection, target);
-      var index = this[collection].indexOf(record);
-      this[collection].splice(index, 1);
-
-    } else if (Ember.isArray(target)) {
-      var records = this._find(collection, target);
-      records.forEach(function(record) {
-        var index = _this[collection].indexOf(record);
-        _this[collection].splice(index, 1);
-      });
-
-    } else if (typeof target === 'object') {
-      var records = this._where(collection, target);
-      records.forEach(function(record) {
-        var index = _this[collection].indexOf(record);
-        _this[collection].splice(index, 1);
-      });
-    }
-  };
-
 
   this.emptyData = function() {
     var _this = this;
@@ -185,4 +54,188 @@ export default function() {
       _this[key] = {};
     });
   };
+
+  this.all = function(collection, data) {
+    var records = this[collection]._records;
+
+    return records.map(function(record) {
+      return JSON.parse(JSON.stringify(record));
+    });
+  };
+
+  this.insert = function(collection, data) {
+    var _this = this;
+    var copy = data ? JSON.parse(JSON.stringify(data)) : {};
+    var records = this[collection]._records;
+    var returnData;
+
+    if (!Ember.isArray(copy)) {
+      var attrs = copy;
+      if (!attrs.id) {
+        attrs.id = records.length + 1;
+      }
+
+      records.push(attrs);
+      returnData = JSON.parse(JSON.stringify(attrs))
+
+    } else {
+      returnData = [];
+      copy.forEach(function(attrs) {
+        if (!attrs.id) {
+          attrs.id = records.length + 1;
+        }
+
+        records.push(attrs);
+        returnData.push(attrs);
+        returnData = returnData.map(function(record) {
+          return JSON.parse(JSON.stringify(record));
+        })
+      });
+    }
+
+    return returnData;
+  };
+
+  this.find = function(collection, ids) {
+    var _this = this;
+    var records;
+
+    if (Ember.isArray(ids)) {
+      var records = this._findRecords(collection, ids)
+        .filter(function(record) {
+          return record !== undefined;
+        });
+
+      // Return a copy
+      return records.map(function(record) {
+        return JSON.parse(JSON.stringify(record));
+      });
+
+    } else {
+      var record = this._findRecord(collection, ids);
+      return JSON.parse(JSON.stringify(record)); // return a copy
+    }
+  };
+
+  this.where = function(collection, query) {
+    var records = this._findRecordsWhere(collection, query);
+
+    return records.map(function(record) {
+      return JSON.parse(JSON.stringify(record));
+    });
+  };
+
+  this.update = function(collection, attrs, target) {
+    if (typeof target === 'undefined') {
+      this[collection]._records.forEach(function(record) {
+        Object.keys(attrs).forEach(function(attr) {
+          record[attr] = attrs[attr];
+        });
+      });
+
+    } else if (typeof target === 'number') {
+      var id = target;
+      var record = this._findRecord(collection, id);
+
+      Object.keys(attrs).forEach(function(attr) {
+        record[attr] = attrs[attr];
+      });
+
+    } else if (Ember.isArray(target)) {
+      var ids = target;
+      var records = this._findRecords(collection, ids);
+
+      records.forEach(function(record) {
+        Object.keys(attrs).forEach(function(attr) {
+          record[attr] = attrs[attr];
+        });
+      });
+
+    } else if (typeof target === 'object') {
+      var query = target;
+      var records = this._findRecordsWhere(collection, query);
+
+      records.forEach(function(record) {
+        Object.keys(attrs).forEach(function(attr) {
+          record[attr] = attrs[attr];
+        });
+      });
+    }
+  };
+
+  this.remove = function(collection, target) {
+    var _this = this;
+    var _collection = this[collection];
+
+    if (typeof target === 'undefined') {
+      _collection._records = [];
+
+    } else if (typeof target === 'number') {
+      var record = this._findRecord(collection, target);
+      var index = _collection._records.indexOf(record);
+      _collection._records.splice(index, 1);
+
+    } else if (Ember.isArray(target)) {
+      var records = this._findRecords(collection, target);
+      records.forEach(function(record) {
+        var index = _collection._records.indexOf(record);
+        _collection._records.splice(index, 1);
+      });
+
+    } else if (typeof target === 'object') {
+      var records = this._findRecordsWhere(collection, target);
+      records.forEach(function(record) {
+        var index = _collection._records.indexOf(record);
+        _collection._records.splice(index, 1);
+      });
+    }
+  };
+
+
+  /*
+    Private methods
+  */
+
+  /*
+    Returns the actual db object.
+  */
+  this._findRecord = function(collection, id) {
+    // If parses, coerce to integer
+    id = parseInt(id, 10) || id;
+
+    var record = this[collection]._records.filter(function(obj) {
+      return obj.id === id;
+    })[0];
+
+    return record;
+  };
+
+  /*
+    Returns the actual db objects.
+  */
+  this._findRecords = function(collection, ids) {
+    var _this = this;
+
+    var records = ids.map(function(id) {
+      return _this._findRecord(collection, id);
+    });
+
+    return records;
+  };
+
+  /*
+    Returns the actual db objects.
+  */
+  this._findRecordsWhere = function(collection, query) {
+    var records = this[collection]._records;
+
+    Object.keys(query).forEach(function(queryKey) {
+      records = records.filter(function(record) {
+        return record[queryKey] === query[queryKey];
+      });
+    });
+
+    return records;
+  };
+
 }
--- tests/unit/db-test.js
+++ tests/unit/db-test.js
@@ -1,4 +1,4 @@
-import Db from 'ember-cli-mirage/db';
+import Db from 'ember-cli-mirage/orm/db';
 
 import {module, test} from 'qunit';
 
@@ -23,14 +23,14 @@ module('mirage:db#createCollection', {
 test('it can create an empty collection', function(assert) {
   db.createCollection('contacts');
 
-  assert.deepEqual(db.contacts, []);
+  assert.ok(db.contacts);
 });
 
 test('it can create many collections', function(assert) {
   db.createCollections('contacts', 'addresses');
 
-  assert.deepEqual(db.contacts, []);
-  assert.deepEqual(db.addresses, []);
+  assert.ok(db.contacts);
+  assert.ok(db.addresses);
 });
 
 
@@ -50,10 +50,37 @@ test('it can load an object of data', function(assert) {
   };
   db.loadData(data);
 
-  assert.deepEqual(db.contacts, data.contacts);
-  assert.deepEqual(db.addresses, data.addresses);
+  assert.deepEqual(db.contacts.all(), data.contacts);
+  assert.deepEqual(db.addresses.all(), data.addresses);
 });
 
+var data = {
+  contacts: [{id: 1, name: 'Link'}],
+  addresses: [{id: 1, name: '123 Hyrule Way'}]
+};
+module('mirage:db#all', {
+  beforeEach: function() {
+    db = new Db();
+    db.loadData(data);
+  },
+  afterEach: function() {
+    db.emptyData();
+  }
+});
+
+test('it can return a collection', function(assert) {
+  assert.deepEqual(db.contacts.all(), data.contacts);
+  assert.deepEqual(db.addresses.all(), data.addresses);
+});
+
+test('the collection is a copy', function(assert) {
+  var contacts = db.contacts.all();
+
+  assert.deepEqual(contacts, data.contacts);
+  contacts[0].name = 'Zelda'
+
+  assert.deepEqual(db.contacts.all(), data.contacts);
+});
 
 module('mirage:db#insert', {
   beforeEach: function() {
@@ -68,35 +95,45 @@ module('mirage:db#insert', {
 test('it inserts an object and returns it', function(assert) {
   var link = db.contacts.insert({name: 'Link'});
 
-  assert.deepEqual(db.contacts, [{id: 1, name: 'Link'}]);
+  assert.deepEqual(db.contacts.all(), [{id: 1, name: 'Link'}]);
   assert.deepEqual(link, {id: 1, name: 'Link'});
+});
+
+test('it returns a copy', function(assert) {
+  var link = db.contacts.insert({name: 'Link'});
+
+  assert.deepEqual(link, {id: 1, name: 'Link'});
+
+  link.name = 'Young link';
+
+  assert.deepEqual(db.contacts.find(1), {id: 1, name: 'Link'});
 });
 
 test('it can insert objects sequentially', function(assert) {
   db.contacts.insert({name: 'Link'});
   db.contacts.insert({name: 'Ganon'});
 
-  assert.deepEqual(db.contacts, [{id: 1, name: 'Link'}, {id: 2, name: 'Ganon'}]);
+  assert.deepEqual(db.contacts.all(), [{id: 1, name: 'Link'}, {id: 2, name: 'Ganon'}]);
 });
 
 test('it does not add an id if present', function(assert) {
   db.contacts.insert({id: 5, name: 'Link'});
 
-  assert.deepEqual(db.contacts, [{id: 5, name: 'Link'}]);
+  assert.deepEqual(db.contacts.all(), [{id: 5, name: 'Link'}]);
 });
 
 test('it can insert an array and return it', function(assert) {
   db.contacts.insert({name: 'Link'});
   var contacts = db.contacts.insert([{name: 'Zelda'}, {name: 'Ganon'}]);
 
-  assert.deepEqual(db.contacts, [{id: 1, name: 'Link'}, {id: 2, name: 'Zelda'}, {id: 3, name: 'Ganon'}]);
+  assert.deepEqual(db.contacts.all(), [{id: 1, name: 'Link'}, {id: 2, name: 'Zelda'}, {id: 3, name: 'Ganon'}]);
   assert.deepEqual(contacts, [{id: 2, name: 'Zelda'}, {id: 3, name: 'Ganon'}]);
 });
 
 test('it does not add ids to array data if present', function(assert) {
   db.contacts.insert([{id: 2, name: 'Link'}, {id: 1, name: 'Ganon'}]);
 
-  assert.deepEqual(db.contacts, [{id: 2, name: 'Link'}, {id: 1, name: 'Ganon'}]);
+  assert.deepEqual(db.contacts.all(), [{id: 2, name: 'Link'}, {id: 1, name: 'Ganon'}]);
 });
 
 
@@ -121,6 +158,16 @@ test('returns a record that matches a numerical id', function(assert) {
   assert.deepEqual(contact, {id: 2, name: 'Link'});
 });
 
+test('returns a copy not a reference', function(assert) {
+  var contact = db.contacts.find(2);
+
+  assert.deepEqual(contact, {id: 2, name: 'Link'});
+
+  contact.name = 'blah';
+
+  assert.deepEqual(db.contacts.find(2), {id: 2, name: 'Link'});
+});
+
 test('coerces interger-like ids to integers', function(assert) {
   var contact = db.contacts.find('2');
 
@@ -139,7 +186,7 @@ test('returns multiple record that match an array of ids', function(assert) {
   assert.deepEqual(contacts, [{id: 1, name: 'Zelda'}, {id: 2, name: 'Link'}]);
 });
 
-test('returns multiple record that match an array of ids', function(assert) {
+test('returns an empty array when it doesnt find multiple ids', function(assert) {
   var contacts = db.contacts.find([99, 100]);
 
   assert.deepEqual(contacts, []);
@@ -169,6 +216,20 @@ test('returns an array of records that match the query', function(assert) {
   ]);
 });
 
+test('returns a copy, not a referecne', function(assert) {
+  var result = db.contacts.where({evil: true});
+
+  assert.deepEqual(result, [
+    {id: 3, name: 'Ganon', evil: true}
+  ]);
+
+  result[0].evil = false;
+
+  assert.deepEqual(db.contacts.where({evil: true}), [
+    {id: 3, name: 'Ganon', evil: true}
+  ]);
+});
+
 test('returns an empty array if no records match the query', function(assert) {
   var result = db.contacts.where({name: 'Link', evil: true});
 
@@ -194,7 +255,7 @@ module('mirage:db#update', {
 test('it can update the whole collection', function(assert) {
   db.contacts.update({name: 'Sam', evil: false});
 
-  assert.deepEqual(db.contacts, [
+  assert.deepEqual(db.contacts.all(), [
     {id: 1, name: 'Sam', evil: false},
     {id: 2, name: 'Sam', evil: false},
     {id: 3, name: 'Sam', evil: false}
@@ -221,7 +282,7 @@ test('it can update multiple records by ids', function(assert) {
 test('it can update records by query', function(assert) {
   db.contacts.update({name: 'Sam'}, {evil: false});
 
-  assert.deepEqual(db.contacts, [
+  assert.deepEqual(db.contacts.all(), [
     {id: 1, name: 'Sam', evil: false},
     {id: 2, name: 'Sam', evil: false},
     {id: 3, name: 'Ganon', evil: true}
@@ -247,13 +308,13 @@ module('mirage:db#remove', {
 test('it can remove an entire collection', function(assert) {
   db.contacts.remove();
 
-  assert.deepEqual(db.contacts, []);
+  assert.deepEqual(db.contacts.all(), []);
 });
 
 test('it can remove a single record', function(assert) {
   db.contacts.remove(1);
 
-  assert.deepEqual(db.contacts, [
+  assert.deepEqual(db.contacts.all(), [
     {id: 2, name: 'Zelda', evil: false},
     {id: 3, name: 'Ganon', evil: true},
   ]);
@@ -262,7 +323,7 @@ test('it can remove a single record', function(assert) {
 test('it can remove multiple records by ids', function(assert) {
   db.contacts.remove([1, 2]);
 
-  assert.deepEqual(db.contacts, [
+  assert.deepEqual(db.contacts.all(), [
     {id: 3, name: 'Ganon', evil: true}
   ]);
 });
@@ -270,7 +331,7 @@ test('it can remove multiple records by ids', function(assert) {
 test('it can remove multiple records by query', function(assert) {
   db.contacts.remove({evil: false});
 
-  assert.deepEqual(db.contacts, [
+  assert.deepEqual(db.contacts.all(), [
     {id: 3, name: 'Ganon', evil: true},
   ]);
 });
